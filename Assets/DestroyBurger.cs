59908cd9395818875cb9c8656360aa647519a158
celpratter <celina.pratter@htl-salzburg.ac.at>
2020-03-11 12:33:00 +0100

    added script on burger for testing if collision works

---
+++ Assets/DestroyBurger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBurger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    void OnTriggerEnter(Collider theCollision) // C#, type first, name in second
+    {
+        if (theCollision.gameObject.tag == "PotTrigger")
+        // By using {}, the condition apply to that entire scope, instead of the next line.
+        {
+            Debug.Log("trigger");
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
