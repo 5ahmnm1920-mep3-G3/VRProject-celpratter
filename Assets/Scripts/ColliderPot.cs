8cf2212483400da3bc006e16d758d4bbc9cc92cd
celpratter <celina.pratter@htl-salzburg.ac.at>
2020-03-11 14:11:39 +0100

    updated ColliderPot script, change pot color to green if healthy - to red if unhealthy

--- Assets/Scripts/ColliderPot.cs
+++ Assets/Scripts/ColliderPot.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class ColliderPot : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Topf;
+
     void OnTriggerEnter(Collider theCollision)
     {
         if (theCollision.gameObject.tag == "Healthy")
         {
             Debug.Log("healthy");
             Destroy(theCollision.gameObject);
+            var TopfRenderer = Topf.GetComponent<Renderer>();
+            TopfRenderer.material.SetColor("_Color", Color.green);
         }
         else
         {
             Debug.Log("unhealthy");
             Destroy(theCollision.gameObject);
+            var TopfRenderer = Topf.GetComponent<Renderer>();
+            TopfRenderer.material.SetColor("_Color", Color.red);
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
 
