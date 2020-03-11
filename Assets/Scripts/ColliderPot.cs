8bae90c079da7ca26b59590a67ecb52225d06a69
celpratter <celina.pratter@htl-salzburg.ac.at>
2020-03-11 12:44:25 +0100

    added script to pot collider, healthy and unhealthy tags

---
+++ Assets/Scripts/ColliderPot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPot : MonoBehaviour
+{
+    void OnTriggerEnter(Collider theCollision)
+    {
+        if (theCollision.gameObject.tag == "Healthy")
+        {
+            Debug.Log("healthy");
+            Destroy(theCollision.gameObject);
+        }
+        else
+        {
+            Debug.Log("unhealthy");
+            Destroy(theCollision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
